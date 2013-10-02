dc2590187e65ca3eac960a15073fcab9fd340324
Junaid Babu <junaidvayoli@gmail.com>
2013-10-03 02:25:50 +0530

    made the entire line to the link

--- Make-Blogger-Archive-Page.js
+++ Make-Blogger-Archive-Page.js
@@ -1,4 +1,4 @@
-function LoadTheArchive(TotalFeed) 
+unction LoadTheArchive(TotalFeed) 
 {
     var PostTitles = new Array();
     var PostURLs = new Array();
@@ -37,6 +37,7 @@ function DisplaytheTOC(PostTitles,PostURLs,PostYears,PostMonths,PostDays)
     for(var EntryNum = 0; EntryNum < NumberOfEntries; EntryNum++)
     {
 	NameOfMonth = MonthNames[parseInt(PostMonths[EntryNum],10)-1]
-	document.write('<a href ="'+PostURLs[EntryNum]+'">'+PostTitles[EntryNum]+"</a> ("+NameOfMonth+" "+parseInt(PostDays[EntryNum],10)+", "+PostYears[EntryNum]+")<br />");
+	document.write('<a href ="'+PostURLs[EntryNum]+'">'+PostTitles[EntryNum]+" ("+NameOfMonth+" "+parseInt(PostDays[EntryNum],10)+", "+PostYears[EntryNum]+")<br /></a>");
+
     }
 }
